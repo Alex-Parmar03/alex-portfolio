1fd5002ffe3fb9df5ce8ea8bc79986bb569232b2
Alex-Parmar03 <136486520+Alex-Parmar03@users.noreply.github.com>
2024-07-02 15:18:22 +0100

    new design

---
+++ assets/js/backtotop.js
@@ -0,0 +1,29 @@
+// Top button:
+let mybutton = document.getElementById("back-to-top");
+
+// nevigation scroll up down
+let prevScrollpos = window.scrollY;
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+
+    // nevigation scroll up down
+    let currentScrollPos = window.scrollY;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-80px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
